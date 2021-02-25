1dfdc1903c8cac8dceadb002cea691160a822eb4
kuhuhalder <kuhuhalder2701@gmail.com>
2021-02-25 00:14:45 -0500

    many changes to part 2 - collection view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kuhu Halder on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
